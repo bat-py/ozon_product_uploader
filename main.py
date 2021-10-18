4d0e4d855980f1501b944d3144b10b18a30d9f37
crow <behruzfox@mail.ru>
2021-10-18 10:54:17 -0400

    csv handler and foto handler are ready

---
+++ main.py
@@ -0,0 +1,80 @@
+import csv
+import os
+from PIL import Image
+from pyzbar.pyzbar import decode
+from uploader import uploader
+
+
+def get_header():
+    """
+    Из файла config.ini берет Client-Id и Api-Key. Вернет библиотеку header = {'Client-Id': data[0], 'Api-Key': data[1]}
+    """
+
+    with open('config.ini') as conf:
+        config = conf.readlines()
+
+        data = [i.split('=')[1].strip().replace('"', '') for i in config]
+        header = {'Client-Id': data[0], 'Api-Key': data[1]}
+
+        return header
+
+
+def get_product_data():
+    with open("positions.csv", encoding='utf-8') as r_file:
+        items_csv = {}
+
+        # Создаем объект reader, указываем символ-разделитель ","
+        file_reader = csv.reader(r_file, delimiter=";")
+        # Счетчик для подсчета количества строк и вывода заголовков столбцов
+        count = 0
+        # Считывание данных из CSV файла
+        for row in file_reader:
+            if count == 0:
+                # Вывод строки, содержащей заголовки для столбцов
+                print(f'Файл содержит столбцы: {", ".join(row)}')
+            else:
+                items_csv[row[3]] = row[1]
+
+            count += 1
+        return items_csv
+
+
+def photos_handler(items_csv):
+    """
+    Вернет dict c данными:
+    {'X120385-c01': ['fotos/Сеанс в студии-1116.jpg', 'fotos/Сеанс в студии-1117.jpg', ...], 'X8496-10-91': [...], ...}
+    Ключ это артикул товара, а в массиве ссылки на фотографии товора
+
+    :param items_csv:
+    :return:
+    """
+
+    fotos = os.listdir('fotos')
+    fotos.sort()
+
+    fotosart = {}
+    group = []
+    for foto in fotos:
+        path = os.path.join('fotos', foto)
+        img = Image.open(path)
+        dec = decode(img)
+        if not dec:
+            group.append(path)
+        else:
+            try:
+                barcode = dec[0].data.decode()
+                article = items_csv[barcode.replace('+', '').lower()]
+
+                fotosart[article] = group
+            finally:
+                group = []
+
+    print(fotosart)
+    return fotosart
+
+if __name__ == '__main__':
+    header = get_header()
+
+    product_data = get_product_data()
+
+    photos_handler(product_data)
